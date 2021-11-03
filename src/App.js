e885e59a2c973fba2bdd692128a853e06a647ecf
Oscar Hernandez <oscarh425@outlook.com>
2021-11-03 14:34:04 -0700

    MVP completed. Cleaning up code remains

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+// import userEvent from "@testing-library/user-event";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import BigComponent from './components/componentOne'
+
+
 
 function App() {
+  const [nasaData, setNasaData] = useState('')
+ //`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+  useEffect( () => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=rnBTrhDY0lTA80dKgBcBESX5zleFVY01aYWrR7vd`)
+    .then(res => {
+      //console.log(res);
+      setNasaData(res.data);
+      console.log(nasaData)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, []);
+
+
+{/* <BigComponent /> */}
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+
+        <BigComponent nasaAPI = {nasaData} />
+
       </p>
     </div>
   );
